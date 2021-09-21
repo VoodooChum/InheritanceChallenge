4751c8271c0a63f385b06e1099a8d920e49a72ae
VoodooChum <brianmillernola@gmail.com>
2021-09-21 10:01:27 -0500

    Composition of Computer

--- src/com/BrianMiller/Main.java
+++ src/com/BrianMiller/Main.java
@@ -3,7 +3,15 @@ package com.BrianMiller;
 public class Main {
 
     public static void main(String[] args) {
-	    RDX tiffanyRDX = new RDX(60);
-        tiffanyRDX.accelerate(30);
+        Dimensions dimensions = new Dimensions(20, 20, 10);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+        Monitor theMonitor = new Monitor("27 inch Beast", "Acer", 27, new Resolution(2540, 1440));
+        Motherboard theMotherboard = new Motherboard("BJ-200", "ASUS", 4, 6, "v2.44");
+
+        PC myPC = new PC(theCase, theMonitor, theMotherboard);
+
+        myPC.getMotherboard().loadProgram("Windows 1.0");
+        myPC.getMonitor().drawPicxelAt(12, 34, "Blue");
     }
+
 }
